ae40c47ae01c933f322efb9d7d34fac3e0e584ef
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-04-29 15:40:04 +0600

    remove public from base_url

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Mulgram Secondary School</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> 
-        <script src="<?= base_url('public/assets/js/bootstrap.bundle.min.js'); ?>"></script>
+        <script src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
         <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
