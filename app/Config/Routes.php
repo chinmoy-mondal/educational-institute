4bd9baf7d43b9e0fb2f8c2c06f16494b54ab2932
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-03 00:10:47 +0600

    modify register routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,32 +5,35 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->setAutoRoute(true); // Old legacy (see below)
-
-// $routes->get('/', 'Home::index');
-// $routes->get('/home', 'Home::index');
-// $routes->get('/home/welcome', 'Home::welcome');
-// $routes->get('/home/history', 'Home::history');
-// $routes->get('/home/mission', 'Home::mission');
-// $routes->get('/home/staff', 'Home::staff');
-// $routes->get('/user', 'User::index');
-
-// $routes->get('database/migrate', 'DatabaseController::migrate');
 
 
-// $routes->get('/contain', 'Contain::index');
-// $routes->get('/about', 'About::index');
-// $routes->get('/exam', 'Home::exam');
-// $routes->get('/result', 'Home::result');
-// $routes->get('/courses', 'Home::courses');
-// $routes->get('/contact', 'Home::contact');
-// $routes->get('/apply', 'Home::apply');
-// $routes->get('/course-details/(:num)', 'Home::courseDetails/$1');
-// $routes->get('/activity-details/(:num)', 'Home::activityDetails/$1');
+$routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
+$routes->get('/home/welcome', 'Home::welcome');
+$routes->get('/home/history', 'Home::history');
+$routes->get('/home/mission', 'Home::mission');
+$routes->get('/home/staff', 'Home::staff');
+$routes->get('/user', 'User::index');
+
+$routes->get('database/migrate', 'DatabaseController::migrate');
+
+
+$routes->get('/contain', 'Contain::index');
+$routes->get('/about', 'About::index');
+$routes->get('/exam', 'Home::exam');
+$routes->get('/result', 'Home::result');
+$routes->get('/courses', 'Home::courses');
+$routes->get('/contact', 'Home::contact');
+$routes->get('/apply', 'Home::apply');
+$routes->get('/course-details/(:num)', 'Home::courseDetails/$1');
+$routes->get('/activity-details/(:num)', 'Home::activityDetails/$1');
 
 
 
-// $routes->get('/page', 'Page::index');
+$routes->get('/page', 'Page::index');
+
+$routes->get('register', 'Register::index');
+$routes->post('register/submit', 'Register::submit');
 
 $routes->set404Override(function () {
     $controller = new \App\Controllers\ErrorController();
