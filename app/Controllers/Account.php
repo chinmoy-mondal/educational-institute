46b054f925a84fabf57c3a323f9ca0e49d8d8df1
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-03 18:53:50 +0600

    change register to account

---
+++ app/Controllers/Account.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Account extends BaseController
+{
+    public function Register()
+    {
+        echo "regi"
+        // return view('regi');
+    }
+    public function Login()
+    {
+        echo "login";
+        // return view('login');
+    }
+
+}
