416c53e3a10c6b7551ef40c65955c626d7a9439e
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-03 19:06:05 +0600

    make view of account

--- app/Controllers/Account.php
+++ app/Controllers/Account.php
@@ -6,13 +6,11 @@ class Account extends BaseController
 {
     public function Register()
     {
-        echo "regi";
-        // return view('regi');
+        return view('register');
     }
     public function Login()
     {
-        echo "login";
-        // return view('login');
+        return view('login');
     }
 
 }
