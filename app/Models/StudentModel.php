500d87fff86aef6ea08c185e72c83ce5f38efba7
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-04 11:57:18 +0600

    add student picture modification

--- app/Models/StudentModel.php
+++ app/Models/StudentModel.php
@@ -6,7 +6,12 @@ class StudentModel extends Model
 {
     protected $table = 'students';
     protected $allowedFields = [
-        'student_name', 'roll', 'class', 'section', 'esif', 'dob', 'phone',
-        'birth_registration_pic', 'father_id_pic', 'mother_id_pic'
-    ];
+    'student_name', 'roll', 'class', 'section', 'esif',
+    'dob', 'phone',
+    'student_pic', // ✅ this must be here
+    'birth_registration_pic',
+    'father_id_pic',
+    'mother_id_pic',
+];
+
 }
