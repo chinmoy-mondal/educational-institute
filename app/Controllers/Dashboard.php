7def1569712ae2328749b62a8e8cc6d28519fc52
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-14 07:56:28 +0600

    login session check

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -7,7 +7,8 @@ class Dashboard extends BaseController
     public function index()
     {
         if (!session()->get('isLoggedIn')) {
-            return redirect()->to('/login');
+            // return redirect()->to('/login');
+            echo "session is not working";
         }
 
         return view('dashboard/index');
