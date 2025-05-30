5ad2e5f7900898835b1fc0b3ee06e3ea74eb4cdf
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-31 00:44:00 +0600

    multiple cart for dashboard

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -58,6 +58,53 @@
         </div>
       </div>
     </div>
+
+
+<div class="row">
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>10</h3>
+            <p>Leave Applications</p>
+          </div>
+          <div class="icon"><i class="fas fa-calendar-alt"></i></div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-success">
+          <div class="inner">
+            <h3>$1200</h3>
+            <p>Payments Collected</p>
+          </div>
+          <div class="icon"><i class="fas fa-dollar-sign"></i></div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-warning">
+          <div class="inner">
+            <h3>3</h3>
+            <p>Results Pending</p>
+          </div>
+          <div class="icon"><i class="fas fa-chart-bar"></i></div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h3>$300</h3>
+            <p>Monthly Expenses</p>
+          </div>
+          <div class="icon"><i class="fas fa-money-bill-wave"></i></div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
