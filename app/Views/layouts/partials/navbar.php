2a25e1f9c9343e91d84fb3266e284707caf7de1d
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-05-30 22:51:29 +0600

    make logout button in right

--- app/Views/layouts/partials/navbar.php
+++ app/Views/layouts/partials/navbar.php
@@ -1,8 +1,23 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Left navbar links -->
   <ul class="navbar-nav">
-    <li class="nav-item"><a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a></li>
-    <li class="nav-item d-none d-sm-inline-block"><a href="#" class="nav-link">Home</a></li>
-    <li class="nav-item d-none d-sm-inline-block"><a href="<?= base_url('dashboard') ?>" class="nav-link">Dashboard</a></li>
-    <li class="nav-item d-none d-sm-inline-block"><a href="<?= base_url('logout') ?>" class="nav-link">Logout</a></li>
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="#" class="nav-link">Home</a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="<?= base_url('dashboard') ?>" class="nav-link">Dashboard</a>
+    </li>
+  </ul>
+
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a href="<?= base_url('logout') ?>" class="nav-link text-danger">
+        <i class="fas fa-sign-out-alt"></i> Logout
+      </a>
+    </li>
   </ul>
 </nav>
