24385577b7c07ae3e1ed3334290c3f72ca171e2b
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-06-26 16:51:27 +0600

    subject link add and subject table

--- app/Views/p_subject.php
+++ app/Views/p_subject.php
@@ -1,5 +1,4 @@
 <?= $this->extend("layouts/base.php") ?>
-
 <?= $this->section("content"); ?>
 
 <!-- Fixed Header -->
@@ -9,53 +8,42 @@
 
 <div class="container content">
 
-<!-- Head Sir Message -->
-<section class="head-sir-message py-5 position-relative">
-    <div class="overlay"></div>
-    <div class="container position-relative text-white text-center">
-        <div class="row align-items-center">
-            <div class="col-lg-4 text-center">
-                <div class="sir-image">
-                    <img src="<?= base_url('public/assets/img/headsir.jpg'); ?>" alt="Head Sir" class="img-fluid">
-                </div>
-            </div>
-            <div class="col-lg-8 text-lg-start">
-                <h2 class="fw-bold">Message from the Head Sir</h2>
-                <p class="fst-italic">"Education is the most powerful weapon which you can use to change the world." – Nelson Mandela</p>
-                <p>
-                    It is my great pleasure to welcome you to our school...
-                </p>
-                <h5 class="fw-bold mt-3">- [Head Sir's Name]</h5>
-                <p class="fst-italic">Head of School</p>
-            </div>
-        </div>
-    </div>
-</section>
-
 <!-- Subject Table Section -->
 <section class="py-5">
     <div class="container">
         <h3 class="text-center mb-4 fw-bold">Subjects Offered (Classes 6 to 9 & Vocational)</h3>
-        <div class="table-responsive">
-            <table class="table table-striped table-bordered align-middle text-center">
-                <thead class="table-dark">
-                    <tr>
-                        <th>Class</th>
-                        <th>Section</th>
-                        <th>Subject</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($subjects as $subject): ?>
+
+        <?php
+        // Group subjects by class
+        $groupedSubjects = [];
+        foreach ($subjects as $subject) {
+            $groupedSubjects[$subject['class']][] = $subject;
+        }
+        ?>
+
+        <?php foreach ($groupedSubjects as $class => $classSubjects): ?>
+            <h4 class="mb-3 mt-5 text-primary fw-bold">Class <?= esc($class) ?></h4>
+            <div class="table-responsive mb-4">
+                <table class="table table-striped table-bordered align-middle text-center">
+                    <thead class="table-dark">
                         <tr>
-                            <td><?= esc($subject['class']) ?></td>
-                            <td><?= esc($subject['section']) ?></td>
-                            <td><?= esc($subject['subject']) ?></td>
+                            <th>Class</th>
+                            <th>Section</th>
+                            <th>Subject</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($classSubjects as $subject): ?>
+                            <tr>
+                                <td><?= esc($subject['class']) ?></td>
+                                <td><?= esc($subject['section']) ?></td>
+                                <td><?= esc($subject['subject']) ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php endforeach; ?>
     </div>
 </section>
 
