3d6a61cbfacf2066b1ce00556a1038f254a8c079
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-07 21:44:36 +0600

    privious CreateStudent table

--- app/Database/Migrations/2025-06-30-024630_CreateStudentsTable.php
+++ app/Database/Migrations/2025-06-30-024630_CreateStudentsTable.php
@@ -10,22 +10,22 @@ class CreateStudents extends Migration
     {
         $this->forge->addField([
             'id'                         => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            'student_name'               => ['type' => 'VARCHAR', 'constraint' => 255],
-            'roll'                       => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
-            'class'                      => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
-            'section'                    => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
-            'esif'                       => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
-            'father_name'                => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
-            'mother_name'                => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'student_name'               => ['type' => 'VARCHAR', 'constraint' => 100],
+            'roll'                       => ['type' => 'VARCHAR', 'constraint' => 20],
+            'class'                      => ['type' => 'VARCHAR', 'constraint' => 10],
+            'section'                    => ['type' => 'VARCHAR', 'constraint' => 10],
+            'esif'                       => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
+            'father_name'                => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
+            'mother_name'                => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
             'dob'                        => ['type' => 'DATE', 'null' => true],
-            'gender'                     => ['type' => 'VARCHAR', 'constraint' => 20, 'null' => true],
-            'phone'                      => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
+            'gender'                     => ['type' => 'ENUM', 'constraint' => ['Male','Female','Other'], 'default' => 'Male'],
+            'phone'                      => ['type' => 'VARCHAR', 'constraint' => 20, 'null' => true],
             'student_pic'                => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
-            'birth_registration_number'  => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
-            'father_nid_number'          => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
-            'mother_nid_number'          => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
-            'religion'                   => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],   // NEW
-            'blood_group'                => ['type' => 'VARCHAR', 'constraint' => 10,  'null' => true],   // NEW
+            'birth_registration_number'  => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
+            'father_nid_number'          => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
+            'mother_nid_number'          => ['type' => 'VARCHAR', 'constraint' => 50, 'null' => true],
+            'religion'                   => ['type' => 'VARCHAR', 'constraint' => 30,  'null' => true],   // NEW
+            'blood_group'                => ['type' => 'VARCHAR', 'constraint' => 5,   'null' => true],   // NEW
             'created_at'                 => ['type' => 'DATETIME', 'null' => true],
             'updated_at'                 => ['type' => 'DATETIME', 'null' => true],
         ]);
@@ -38,4 +38,4 @@ class CreateStudents extends Migration
     {
         $this->forge->dropTable('students');
     }
-} 
+}
