c8bfd8be058947232494a6db6c8596257c67a435
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-07 17:09:24 +0600

    check include to extend

--- app/Views/public/home.php
+++ app/Views/public/home.php
@@ -5,23 +5,23 @@ sdfsd
 
      <!--  Fixed Wrapper for Navbar -->
         <div class="fixed-header">
-            <?= $this->include("layouts/base-structure/header"); ?>
+            <?= $this->extend("layouts/base-structure/header"); ?>
         </div>
         <div class="container content">
-            <?= $this->include("layouts/base-structure/slider"); ?>
+            <?= $this->extend("layouts/base-structure/slider"); ?>
         </div>
 
         <!--  Popular Courses Section -->
         <div class="popular-courses py-5">
             <div class="container">
-                <?= $this->include("layouts/base-structure/section1"); ?>
+                <?= $this->extend("layouts/base-structure/section1"); ?>
             </div>
             <div class="container">
-                <?= $this->include("layouts/base-structure/section2"); ?>
+                <?= $this->extend("layouts/base-structure/section2"); ?>
             </div>
         </div>
 
-        <?= $this->include("layouts/base-structure/footer"); ?>
+        <?= $this->extend("layouts/base-structure/footer"); ?>
 
 <?= $this->endSection(); ?>
   
