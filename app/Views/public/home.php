114baa6ea18c186b8ad9dd95c32a98634f8e0e43
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-07 17:12:28 +0600

    check

--- app/Views/public/home.php
+++ app/Views/public/home.php
@@ -1,27 +1,26 @@
-sdfsd
-<?= $this->include("layouts/base.php") ?>
+<?= $this->extend("layouts/base.php") ?>
 
-<?= $this->section("content"); ?>
+<?= $this->section("content") ?>
 
-     <!--  Fixed Wrapper for Navbar -->
-        <div class="fixed-header">
-            <?= $this->extend("layouts/base-structure/header"); ?>
+    <!-- Fixed Wrapper for Navbar -->
+    <div class="fixed-header">
+        <?= $this->include("layouts/base-structure/header") ?>
+    </div>
+
+    <div class="container content">
+        <?= $this->include("layouts/base-structure/slider") ?>
+    </div>
+
+    <!-- Popular Courses Section -->
+    <div class="popular-courses py-5">
+        <div class="container">
+            <?= $this->include("layouts/base-structure/section1") ?>
         </div>
-        <div class="container content">
-            <?= $this->extend("layouts/base-structure/slider"); ?>
+        <div class="container">
+            <?= $this->include("layouts/base-structure/section2") ?>
         </div>
+    </div>
 
-        <!--  Popular Courses Section -->
-        <div class="popular-courses py-5">
-            <div class="container">
-                <?= $this->extend("layouts/base-structure/section1"); ?>
-            </div>
-            <div class="container">
-                <?= $this->extend("layouts/base-structure/section2"); ?>
-            </div>
-        </div>
+    <?= $this->include("layouts/base-structure/footer") ?>
 
-        <?= $this->extend("layouts/base-structure/footer"); ?>
-
-<?= $this->endSection(); ?>
-  
+<?= $this->endSection() ?>
