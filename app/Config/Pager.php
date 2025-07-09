179883d978a858f31c156e3768016fa18b5e90ec
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 07:20:16 +0600

    commit

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -25,7 +25,7 @@ class Pager extends BaseConfig
 		'default_full'   => 'CodeIgniter\Pager\Views\default_full',
 		'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
 		'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-		'bootstrap'      => 'app\Views\pager\bootstrap',
+		'bootstrap'      => 'app\Views\Pager\bootstrap',
 	];
 	/**
 	 * --------------------------------------------------------------------------
