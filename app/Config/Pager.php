8c14bb38effb32ecef730679ffcbee0e8fda5586
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 08:16:04 +0600

    update code

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -1,14 +1,36 @@
 <?php
+
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
 
 class Pager extends BaseConfig
 {
-    public int $perPage = 10;
+	/**
+	 * --------------------------------------------------------------------------
+	 * Templates
+	 * --------------------------------------------------------------------------
+	 *
+	 * Pagination links are rendered out using views to configure their
+	 * appearance. This array contains aliases and the view names to
+	 * use when rendering the links.
+	 *
+	 * Within each view, the Pager object will be available as $pager,
+	 * and the desired group as $pagerGroup;
+	 *
+	 * @var array<string, string>
+	 */
 
-    public array $templates = [
-        'default' => 'App\Views\Pager\bootstrap', // use \ not /
-        'bootstrap' => 'App\Views\Pager\bootstrap',
-    ];
+	public $templates = [
+		'default'   => 'App\Views\pagers\bootstrap',
+		'bootstrap' => 'App\Views\pagers\bootstrap',
+	];
+	/**
+	 * --------------------------------------------------------------------------
+	 * Items Per Page
+	 * --------------------------------------------------------------------------
+	 *
+	 * The default number of results shown in a single page.
+	 */
+	public int $perPage = 20;
 }
