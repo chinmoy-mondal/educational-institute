30cc09b4b6abf10e2fb9417a18296fc40b7fc85c
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 07:12:12 +0600

    git code

--- app/Views/public/student_portal.php
+++ app/Views/public/student_portal.php
@@ -77,7 +77,7 @@
                 <!-- Pagination -->
                 <?php if (!empty($pager)): ?>
                     <div class="mt-4">
-		    <?= $pager->only(['q', 'class', 'section'])->links('default', 'pagers/bootstrap') ?>
+		    <?= $pager->only(['q', 'class', 'section'])->links('default', 'bootstrap') ?>
                     </div>
                 <?php endif ?>
             </div>
