967a2bad99032d4056e790ca57ccc0fd2a0f91fb
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 09:02:20 +0600

    add bootstrap pagination

--- app/Views/pagers/bootstrap.php
+++ app/Views/pagers/bootstrap.php
@@ -1,23 +1,31 @@
-<?php if ($pager->hasPrevious() || $pager->hasNext()): ?>
-<nav aria-label="Page navigation">
-    <ul class="pagination justify-content-center">
-        <?php if ($pager->hasPrevious()): ?>
-            <li class="page-item">
-                <a class="page-link" href="<?= $pager->getPreviousPage() ?>">Previous</a>
-            </li>
-        <?php endif; ?>
+<?php if ($pager->hasPrevious() || $pager->hasNext() || count($pager->links())) : ?>
+<div class="container mt-3">
+  <ul class="pagination justify-content-center">
 
-        <?php foreach ($pager->links() as $link): ?>
-            <li class="page-item <?= $link['active'] ? 'active' : '' ?>">
-                <a class="page-link" href="<?= $link['uri'] ?>"><?= $link['title'] ?></a>
-            </li>
-        <?php endforeach ?>
+    <?php if ($pager->hasPrevious()) : ?>
+      <li class="page-item">
+        <a class="page-link" href="<?= $pager->getPreviousPage() ?>" aria-label="Previous">
+          &laquo; Previous
+        </a>
+      </li>
+    <?php endif; ?>
 
-        <?php if ($pager->hasNext()): ?>
-            <li class="page-item">
-                <a class="page-link" href="<?= $pager->getNextPage() ?>">Next</a>
-            </li>
-        <?php endif; ?>
-    </ul>
-</nav>
-<?php endif ?>
+    <?php foreach ($pager->links() as $link) : ?>
+      <li class="page-item <?= $link['active'] ? 'active' : '' ?>">
+        <a class="page-link" href="<?= $link['uri'] ?>">
+          <?= $link['title'] ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+
+    <?php if ($pager->hasNext()) : ?>
+      <li class="page-item">
+        <a class="page-link" href="<?= $pager->getNextPage() ?>" aria-label="Next">
+          Next &raquo;
+        </a>
+      </li>
+    <?php endif; ?>
+
+  </ul>
+</div>
+<?php endif; ?>
