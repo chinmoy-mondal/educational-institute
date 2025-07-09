6a70cb3061782ed606a65647f24724c0d50d45ff
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 07:16:41 +0600

    change pagers to page

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -25,7 +25,7 @@ class Pager extends BaseConfig
 		'default_full'   => 'CodeIgniter\Pager\Views\default_full',
 		'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
 		'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-		'bootstrap'      => 'app\Views\pagers\bootstrap',
+		'bootstrap'      => 'app\Views\pager\bootstrap',
 	];
 	/**
 	 * --------------------------------------------------------------------------
