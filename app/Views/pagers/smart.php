7c8797d6868bb3d593686700e30c884b52b5a28c
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 06:11:44 +0600

    modify upto middle

--- app/Views/pagers/smart.php
+++ app/Views/pagers/smart.php
@@ -1,53 +1,51 @@
-<?php if ($pager->links('default')): ?>
-    <nav>
-        <ul class="pagination justify-content-center">
-            <!-- First & Previous -->
-            <?php if ($pager->hasPrevious('default')): ?>
-                <li class="page-item">
-                    <a class="page-link" href="<?= $pager->getFirst('default') ?>">First</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="<?= $pager->getPreviousPage('default') ?>">&laquo;</a>
-                </li>
-            <?php endif ?>
+<?php if ($pager->hasPrevious() || $pager->hasNext()): ?>
+  <?php
+    $segment = 'default';
+    $currentPage = $pager->getCurrentPage($segment);
+    $totalPages  = $pager->getPageCount($segment);
+    $surround = 5; // 5 pages before and after
+    $start = max(1, $currentPage - $surround);
+    $end   = min($totalPages, $currentPage + $surround);
+  ?>
+  <nav>
+    <ul class="pagination justify-content-center">
 
-            <?php
-                $currentPage = $pager->getCurrentPage('default');
-                $pageCount   = $pager->getPageCount('default');
-                $maxLinks    = 10;
-                $start = max(1, $currentPage - floor($maxLinks / 2));
-                $end   = min($pageCount, $start + $maxLinks - 1);
-                if ($end - $start < $maxLinks - 1) {
-                    $start = max(1, $end - $maxLinks + 1);
-                }
-            ?>
+      <!-- First & Prev -->
+      <?php if ($pager->hasPrevious($segment)): ?>
+        <li class="page-item">
+          <a class="page-link" href="<?= $pager->getFirst($segment) ?>">First</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="<?= $pager->getPreviousPage($segment) ?>">&laquo;</a>
+        </li>
+      <?php endif ?>
 
-            <!-- Left ellipsis -->
-            <?php if ($start > 1): ?>
-                <li class="page-item disabled"><span class="page-link">…</span></li>
-            <?php endif ?>
+      <!-- Left dots -->
+      <?php if ($start > 1): ?>
+        <li class="page-item disabled"><span class="page-link">…</span></li>
+      <?php endif ?>
 
-            <!-- Page number buttons -->
-            <?php foreach (range($start, $end) as $i): ?>
-                <li class="page-item <?= ($i == $currentPage) ? 'active' : '' ?>">
-                    <a class="page-link" href="<?= $pager->getPageURI($i, 'default') ?>"><?= $i ?></a>
-                </li>
-            <?php endforeach ?>
+      <!-- Page Numbers -->
+      <?php for ($i = $start; $i <= $end; $i++): ?>
+        <li class="page-item <?= ($i == $currentPage) ? 'active' : '' ?>">
+          <a class="page-link" href="<?= $pager->getPageURI($i, $segment) ?>"><?= $i ?></a>
+        </li>
+      <?php endfor ?>
 
-            <!-- Right ellipsis -->
-            <?php if ($end < $pageCount): ?>
-                <li class="page-item disabled"><span class="page-link">…</span></li>
-            <?php endif ?>
+      <!-- Right dots -->
+      <?php if ($end < $totalPages): ?>
+        <li class="page-item disabled"><span class="page-link">…</span></li>
+      <?php endif ?>
 
-            <!-- Next & Last -->
-            <?php if ($pager->hasNext('default')): ?>
-                <li class="page-item">
-                    <a class="page-link" href="<?= $pager->getNextPage('default') ?>">&raquo;</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="<?= $pager->getLast('default') ?>">Last</a>
-                </li>
-            <?php endif ?>
-        </ul>
-    </nav>
+      <!-- Next & Last -->
+      <?php if ($pager->hasNext($segment)): ?>
+        <li class="page-item">
+          <a class="page-link" href="<?= $pager->getNextPage($segment) ?>">&raquo;</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="<?= $pager->getLast($segment) ?>">Last</a>
+        </li>
+      <?php endif ?>
+    </ul>
+  </nav>
 <?php endif ?>
