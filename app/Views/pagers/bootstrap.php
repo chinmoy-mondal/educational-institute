1e644a7688c0766286969a1f8242ea228f93ca03
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-09 06:32:20 +0600

    add

--- app/Views/pagers/bootstrap.php
+++ app/Views/pagers/bootstrap.php
@@ -1,44 +1,42 @@
 <?php if ($pager->hasPages()): ?>
-<nav aria-label="Page navigation">
-    <ul class="pagination justify-content-center">
+    <nav aria-label="Pagination">
+        <ul class="pagination justify-content-center">
+            <!-- First -->
+            <?php if ($pager->hasPrevious()): ?>
+                <li class="page-item">
+                    <a class="page-link" href="<?= $pager->getFirst() ?>">First</a>
+                </li>
+            <?php endif; ?>
 
-        <!-- First -->
-        <?php if ($pager->hasPrevious()): ?>
-            <li class="page-item">
-                <a class="page-link" href="<?= $pager->getFirst() ?>">First</a>
-            </li>
-        <?php endif ?>
+            <!-- Previous -->
+            <?php if ($pager->hasPrevious()): ?>
+                <li class="page-item">
+                    <a class="page-link" href="<?= $pager->getPreviousPage() ?>">&laquo;</a>
+                </li>
+            <?php endif; ?>
 
-        <!-- Previous -->
-        <?php if ($pager->hasPrevious()): ?>
-            <li class="page-item">
-                <a class="page-link" href="<?= $pager->getPreviousPage() ?>">&laquo;</a>
-            </li>
-        <?php endif ?>
+            <!-- Page Numbers -->
+            <?php foreach ($pager->links() as $link): ?>
+                <li class="page-item <?= $link['active'] ? 'active' : '' ?>">
+                    <a class="page-link" href="<?= $link['uri'] ?>">
+                        <?= $link['title'] ?>
+                    </a>
+                </li>
+            <?php endforeach ?>
 
-        <!-- Page Numbers -->
-        <?php foreach ($pager->links() as $link): ?>
-            <li class="page-item <?= $link['active'] ? 'active' : '' ?>">
-                <a class="page-link" href="<?= $link['uri'] ?>">
-                    <?= $link['title'] ?>
-                </a>
-            </li>
-        <?php endforeach ?>
+            <!-- Next -->
+            <?php if ($pager->hasNext()): ?>
+                <li class="page-item">
+                    <a class="page-link" href="<?= $pager->getNextPage() ?>">&raquo;</a>
+                </li>
+            <?php endif; ?>
 
-        <!-- Next -->
-        <?php if ($pager->hasNext()): ?>
-            <li class="page-item">
-                <a class="page-link" href="<?= $pager->getNextPage() ?>">&raquo;</a>
-            </li>
-        <?php endif ?>
-
-        <!-- Last -->
-        <?php if ($pager->hasNext()): ?>
-            <li class="page-item">
-                <a class="page-link" href="<?= $pager->getLast() ?>">Last</a>
-            </li>
-        <?php endif ?>
-
-    </ul>
-</nav>
+            <!-- Last -->
+            <?php if ($pager->hasNext()): ?>
+                <li class="page-item">
+                    <a class="page-link" href="<?= $pager->getLast() ?>">Last</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </nav>
 <?php endif ?>
