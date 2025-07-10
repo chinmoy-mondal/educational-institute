e4795b74731fa305c1261fd013d5134581bebed7
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-10 07:45:26 +0600

    test template

--- app/Views/pagers/bootstrap.php
+++ app/Views/pagers/bootstrap.php
@@ -1,31 +1,7 @@
-<?php if ($pager->hasPrevious() || $pager->hasNext() || count($pager->links())) : ?>
-<div class="container mt-3">
-  <ul class="pagination justify-content-center">
-
-    <?php if ($pager->hasPrevious()) : ?>
-      <li class="page-item">
-        <a class="page-link" href="<?= $pager->getPreviousPage() ?>" aria-label="Previous">
-          &laquo; Previous
-        </a>
-      </li>
-    <?php endif; ?>
-
-    <?php foreach ($pager->links() as $link) : ?>
-      <li class="page-item <?= $link['active'] ? 'active' : '' ?>">
-        <a class="page-link" href="<?= $link['uri'] ?>">
-          <?= $link['title'] ?>
-        </a>
-      </li>
-    <?php endforeach; ?>
-
-    <?php if ($pager->hasNext()) : ?>
-      <li class="page-item">
-        <a class="page-link" href="<?= $pager->getNextPage() ?>" aria-label="Next">
-          Next &raquo;
-        </a>
-      </li>
-    <?php endif; ?>
-
-  </ul>
-</div>
-<?php endif; ?>
+<ul class="pagination">
+  <li><a href="#">1</a></li>
+  <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+</ul>
--- app/Views/public/student_portal.php
+++ app/Views/public/student_portal.php
@@ -74,7 +74,7 @@
                     <!-- Pagination -->
                     <?php if (!empty($pager)): ?>
                         <div class="mt-4">
-                            <?= $pager->only(['q', 'class', 'section'])->links('default_simple') ?>
+                            <?= $pager->only(['q', 'class', 'section'])->links('bootstrap') ?>
                         </div>
                     <?php endif; ?>
 
