3217fcec93779a835a233c55597c94ae55de54d9
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-17 09:22:47 +0600

    left sidebar modify

--- app/Views/layouts/admin-structure/sidebar.php
+++ app/Views/layouts/admin-structure/sidebar.php
@@ -1,3 +1,14 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <a href="<?= base_url('dashboard') ?>" class="brand-link">
+    <span class="brand-text font-weight-light"><?= esc($title) ?></span>
+  </a>
+  <div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
+
+
+
+
 <?php foreach ($sidebarItems ?? [] as $item): ?>
   <li class="nav-item">
     <a href="<?= $item['url'] ?>"
@@ -7,3 +18,9 @@
     </a>
   </li>
 <?php endforeach; ?>
+	  <!-- Add more sidebar items similarly -->
+	</ul>
+    </nav>
+  </div>
+</aside>
+
