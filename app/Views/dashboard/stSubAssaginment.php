e42a19d4c5d2f328f37025ded70a2d36c277466c
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-07-20 13:06:10 +0600

    add seperation

--- app/Views/dashboard/stSubAssaginment.php
+++ app/Views/dashboard/stSubAssaginment.php
@@ -67,7 +67,14 @@
 	      <select multiple class="form-control select2" id="leftSelect" name="left_select[]" style="height: 400px; width: 100%;">
 		<?php foreach ($students as $s): ?>
 		  <option value="<?= esc($s['id']) ?>">
-		    <pre><?= esc($s['class']) . "\t" . esc($s['roll']) . "\t" . esc($s['student_name']) . "\t" . esc($s['section']) ?></pre>
+		    <?php foreach ($students as $index => $s): ?>
+			  <option value="<?= esc($s['id']) ?>">
+			    <?= esc($s['class']) ?> - <?= esc($s['roll']) ?> - <?= esc($s['student_name']) ?> - <?= esc($s['section']) ?>
+			  </option>
+			  <?php if ($index !== array_key_last($students)): ?>
+			    <option disabled>────────────</option> <!-- Spacer -->
+			  <?php endif ?>
+			<?php endforeach ?>
 		  </option>
 		<?php endforeach ?>
 	      </select>
