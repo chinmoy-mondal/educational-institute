af8fe8c7fa3d30b309e6eb8dd94eb76fa7252f49
chinmoy monda <chinmoymondal791@gmail.com>
2025-07-24 11:02:40 +0600

    below

--- app/Views/dashboard/ad_new_user.php
+++ app/Views/dashboard/ad_new_user.php
@@ -1,28 +1,18 @@
 
+
+<?= $this->include("layouts/admin") ?>
+
+
 <?php 
 if (!empty($newUsers)) {
     foreach ($newUsers as $user) {
         echo "Name: " . $user['name'] . "<br>";
-        echo "Designation: " . $user['designation'] . "<br>";
-        echo "Subject: " . $user['subject'] . "<br>";
-        echo "Email: " . $user['email'] . "<br>";
-        echo "Phone: " . $user['phone'] . "<br>";
-        echo "Gender: " . ucfirst($user['gender']) . "<br>";
-        echo "Photo URL: " . (!empty($user['photo']) 
-            ? base_url('uploads/' . $user['photo']) 
-            : base_url('public/assets/img/default.png')) . "<br>";
-        echo "Permit URL: " . base_url('user_permit/' . $user['id']) . "<br>";
-        echo "Delete URL: " . base_url('user_delete/' . $user['id']) . "<br>";
-        echo "-----------------------------<br><br>";
     }
 } else {
     echo "No new users found.<br>";
 }
 
 ?>
-
-
-<?= $this->include("layouts/admin") ?>
 <?= $this->section("content") ?>
 
 <section class="content">
@@ -47,34 +37,6 @@ if (!empty($newUsers)) {
                 <th>Action</th>
               </tr>
             </thead>
-<tbody>
-    <?php if (!empty($newUsers)) : ?>
-        <?php foreach ($newUsers as $user) : ?>
-            <tr>
-                <td>
-                    <img src="<?= !empty($user['photo']) 
-                        ? base_url('uploads/' . $user['photo']) 
-                        : base_url('public/assets/img/default.png') ?>" 
-                        alt="User Photo" width="50">
-                </td>
-                <td><?= esc($user['name']) ?></td>
-                <td><?= esc($user['designation']) ?></td>
-                <td><?= esc($user['subject']) ?></td>
-                <td><?= esc($user['email']) ?></td>
-                <td><?= esc($user['phone']) ?></td>
-                <td><?= ucfirst(esc($user['gender'])) ?></td>
-                <td>
-                    <a href="<?= base_url('user_permit/' . $user['id']) ?>" class="btn btn-success btn-sm">Permit</a>
-                    <a href="<?= base_url('user_delete/' . $user['id']) ?>" class="btn btn-danger btn-sm">Delete</a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <tr>
-            <td colspan="8" class="text-center">No new users found.</td>
-        </tr>
-    <?php endif; ?>
-</tbody>
 
 
 
