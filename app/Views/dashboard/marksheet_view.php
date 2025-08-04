892130959f5922bd8ba87072da67e54ab4d4f3c0
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-08-05 00:04:46 +0600

    show student name

--- app/Views/dashboard/marksheet_view.php
+++ app/Views/dashboard/marksheet_view.php
@@ -152,7 +152,7 @@ td strong {
 <!-- Student + Exam Info -->
   <table class="student-info">
     <tr>
-      <td><strong>Student's Name</strong>: <?= esc($student['name']) ?></td>
+      <td><strong>Student's Name</strong>: ?= esc($student['student_name']) ?></td>
     </tr>
     <tr>
       <td><strong>Father's Name</strong>: Kazi Mahmudul Islam</td>
