f79a929cdb2d8c54ffe3046ac21d9d2c67cf8760
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-08-11 21:21:12 +0600

    admin card

--- app/Controllers/AdmitCard.php
+++ app/Controllers/AdmitCard.php
@@ -8,27 +8,27 @@ use App\Models\CalendarModel;
 
 class AdmitCard extends BaseController
 {
-    public function index($class = null)
-    {
-        $studentModel = new StudentModel();
-        $eventModel = new CalendarModel();
+	public function index($class = null)
+	{
+		$studentModel = new StudentModel();
+		$eventModel = new CalendarModel();
 
-	if($class !== null) {
-		$students = $studentModel
-				->where('class',$class)
-				->orderBy('roll','ASC')
+		if ($class !== null) {
+			$students = $studentModel
+				->where('class', $class)
+				->orderBy('roll', 'ASC')
 				->findAll();
-		$events = $eventModel->findAll();
-	} else {
-		$students = $studentModel
-				->where('class !=','10')
-				->orderBy('class','ASC')
+			$events = $eventModel->findAll();
+		} else {
+			$students = $studentModel
+				->where('class !=', '10')
+				->orderBy('class', 'ASC')
 				->findAll();
-		$events = $eventModel->findAll();
+			$events = $eventModel->findAll();
+		}
+		return view('public/admit_card', [
+			'students' => $students,
+			'events' => $events
+		]);
 	}
-        return view('admit_card', [
-            'students' => $students,
-            'events' => $events
-        ]);
-    }
 }
