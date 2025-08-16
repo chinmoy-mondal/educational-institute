2847f5df4131cebd7c5501844d4d87145be98843
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-08-16 11:20:45 +0600

    usr field modify

---
+++ app/Database/Migrations/2025-08-16-051725_AddFieldsToUsersTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'index_number' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+                'null' => true,
+            ],
+            'dob' => [
+                'type' => 'DATE',
+                'null' => true,
+            ],
+            'joining_date' => [
+                'type' => 'DATE',
+                'null' => true,
+            ],
+            'religion' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+                'null' => true,
+            ],
+        ];
+
+        $this->forge->addColumn('users', $fields);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('users', ['index_number', 'dob', 'joining_date', 'religion']);
+    }
+}
