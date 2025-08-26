e3a8d993444d1cdbeb297adaf7f79fe60fae6e2f
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-08-26 21:00:13 +0600

    modify public calendar1

--- app/Controllers/PublicCalendar.php
+++ app/Controllers/PublicCalendar.php
@@ -19,12 +19,18 @@ class PublicCalendar extends Controller
 
         $data = array_map(function ($event) {
             return [
-                'id'    => $event['id'],
-                'title' => $event['title'],
-                'start' => $event['start_date'],
-                'end'   => date('Y-m-d', strtotime($event['end_date'] . ' +1 day')),
-                'color' => $event['color'],
-                'description' => $event['description']
+                'id'          => $event['id'],
+                'title'       => $event['title'],
+                'description' => $event['description'],
+                'start'       => $event['start_date'],
+                'start_time'  => $event['start_time'],
+                'end'         => date('Y-m-d', strtotime($event['end_date'] . ' +1 day')),
+                'end_time'    => $event['end_time'],
+                'color'       => $event['color'],
+                'category'    => $event['category'],
+                'subcategory' => $event['subcategory'],
+                'class'       => $event['class'],
+                'subject'     => $event['subject']
             ];
         }, $events);
 
