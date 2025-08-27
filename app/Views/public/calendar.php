e2ca8077ca7830b05001d06fd60945a2305f0c80
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-08-27 06:54:17 +0600

    modify all7

--- app/Views/public/calendar.php
+++ app/Views/public/calendar.php
@@ -87,8 +87,8 @@
 
                 // Populate modal
                 document.getElementById("modal-title").innerText = info.event.title || "";
-                document.getElementById("modal-desc").innerText = info.event.extendedProps.subject || "";
-                document.getElementById("modal-subject").innerText = info.event.extendedProps.color || "dfsdfsdf";
+                document.getElementById("modal-desc").innerText = info.event.extendedProps.description || "";
+                document.getElementById("modal-subject").innerText = info.event.extendedProps.description || "dfsdfsdf";
                 document.getElementById("modal-date").innerText = startDate;
                 document.getElementById("modal-start").innerText = startTime;
                 document.getElementById("modal-end").innerText = endTime;
