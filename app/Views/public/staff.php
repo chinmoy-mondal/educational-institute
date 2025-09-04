9920cc0cbe76e1b2921e2a20a8d50b13ad2a36ce
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-09-04 07:04:53 +0600

    add profile view button2

--- app/Views/public/staff.php
+++ app/Views/public/staff.php
@@ -27,7 +27,7 @@
                 <?php if (!empty($faculty)): ?>
                     <?php foreach ($faculty as $member): ?>
                         <div class="col-md-4">
-                            <a href="<?= base_url('/user-id?q=' . $member['id']) ?>" class="no-underline">
+                            <a href="<?= base_url('/user-id?q=' . $member['id']) ?>" style="text-decoration: none;">
                             <div class="card h-100 text-center border-0 shadow-sm faculty-card">
                                 <!-- Profile Image -->
                                 <div class="p-3">
