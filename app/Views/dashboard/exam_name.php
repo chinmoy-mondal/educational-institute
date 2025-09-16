9fd65b8f99ec59ad94f0ebf1af55bc25e26ba326
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-09-16 18:32:59 +0600

    re designe 1

--- app/Views/dashboard/exam_name.php
+++ app/Views/dashboard/exam_name.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layouts/admin') ?>
 
 <?= $this->section('content') ?>
-<div class="content-wrapper d-flex justify-content-center" style="padding-top: 50px;">
+<div class=" d-flex justify-content-center" style="padding-top: 20px;">
     <div class="card shadow-lg p-4" style="width: 500px;">
         <div class="card-header text-center bg-primary text-white">
             <h4 class="mb-0">Select Exam</h4>
