9bb0eabac7f2d24c33ddd3de8b91f206b0451d6f
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-09-16 20:10:42 +0600

    give greate condition for Test and pre test for class 10 or not 3

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1005,8 +1005,8 @@ class Dashboard extends Controller
 		$user    = $this->userModel->find($userId);
 		$subject = $this->subjectModel->find($subjectId);
 		print_r($subject);
-		$class = $subject->class;
-		print_r($class);
+		// $class = $subject->class;
+		// print_r($class);
 
 		if (!$user) {
 			return redirect()->back()->with('error', 'User data is not Found.');
