872943d2d91a1fa4340209bb3aa62b584846d395
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-09-22 14:48:10 +0600

    test table data 3

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -473,7 +473,7 @@ class Dashboard extends Controller
 
 				// 2️⃣ Users assigned to this subject
 				$users = $this->userModel
-					->like('assign_sub', $subjectId) // matches if subjectId exists in assign_sub string
+					->like('assagin_sub', $subjectId) // matches if subjectId exists in assign_sub string
 					->findAll();
 
 				// 3️⃣ Results for this subject, exam, and year
