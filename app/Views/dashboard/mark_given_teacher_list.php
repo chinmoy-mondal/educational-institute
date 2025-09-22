9b5d076a83ec89bcfde71ccb0c02dc31a2d2d113
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-09-22 07:41:35 +0600

    mark given teacher list6

--- app/Views/dashboard/mark_given_teacher_list.php
+++ app/Views/dashboard/mark_given_teacher_list.php
@@ -55,30 +55,28 @@
                         <div class="progress-bar bg-success" style="width: 70%;">70%</div>
                       </div>
                     </td>
-                    <td class="text-center">
-                      <div class="btn-group" role="group">
-                        <!-- Profile Button -->
-                        <a href="<?= site_url('profile_id/' . $user['id']) ?>"
-                          class="btn btn-sm btn-info"
-                          title="View Profile">
-                          <i class="fas fa-user"></i>
-                        </a>
+<td class="text-center">
+  <!-- Profile Button -->
+  <a href="<?= site_url('profile_id/' . $user['id']) ?>"
+     class="btn btn-sm btn-info me-1"
+     title="View Profile">
+    <i class="fas fa-user"></i>
+  </a>
 
-                        <!-- Call Button -->
-                        <a href="tel:<?= esc($user['phone'] ?? '') ?>"
-                          class="btn btn-sm btn-success"
-                          title="Call Teacher">
-                          <i class="fas fa-phone"></i>
-                        </a>
+  <!-- Call Button -->
+  <a href="tel:<?= esc($user['phone'] ?? '') ?>"
+     class="btn btn-sm btn-success me-1"
+     title="Call Teacher">
+    <i class="fas fa-phone"></i>
+  </a>
 
-                        <!-- Result View Button -->
-                        <a href="<?= site_url('teacher_result/' . $user['id']) ?>"
-                          class="btn btn-sm btn-warning"
-                          title="View Results">
-                          <i class="fas fa-chart-bar"></i>
-                        </a>
-                      </div>
-                    </td>
+  <!-- Result View Button -->
+  <a href="<?= site_url('teacher_result/' . $user['id']) ?>"
+     class="btn btn-sm btn-warning"
+     title="View Results">
+    <i class="fas fa-chart-bar"></i>
+  </a>
+</td>
                   </tr>
                 <?php endforeach; ?>
               <?php else: ?>
