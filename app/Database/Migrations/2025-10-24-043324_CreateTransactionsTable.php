e8b0f14205371a9f85a48bbe0ec416ac8e4e9562
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-10-24 10:34:42 +0600

    add migration

---
+++ app/Database/Migrations/2025-10-24-043324_CreateTransactionsTable.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'transaction_id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'unique'     => true,
+                'null'       => false,
+            ],
+            'sender_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'sender_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'receiver_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'receiver_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'amount' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+                'default'    => '0.00',
+            ],
+            'purpose' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+                'null'       => true,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'status' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 0, // default pending
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => false,
+                'default' => 'CURRENT_TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type'      => 'DATETIME',
+                'null'      => false,
+                'default'   => 'CURRENT_TIMESTAMP',
+                'on update' => 'CURRENT_TIMESTAMP',
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('transactions', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('transactions', true);
+    }
+}
