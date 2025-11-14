f6f6ce6c46be7ca7c78aaa72d1c740bbc4c0974c
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-11-14 15:11:34 +0600

    try to change the button

--- app/Views/dashboard/tec_pay.php
+++ app/Views/dashboard/tec_pay.php
@@ -4,27 +4,32 @@
 <div class="container-fluid">
     <h3 class="mb-4">Teacher Earnings Dashboard</h3>
 
-<div class="row">
-    <?php foreach ($teachers as $t): ?>
-        <div class="col-lg-2 col-6 mb-3">
-            <div class="small-box <?= $t['unpaid'] > 0 ? 'bg-warning' : 'bg-success' ?>">
-                <div class="inner text-center">
-                    <h4>৳ <?= number_format($t['total_earned'], 2) ?></h4>
-                    <p><?= esc($t['name']) ?></p>
+    <div class="row">
+        <?php foreach ($teachers as $t): ?>
+            <div class="col-lg-2 col-6 mb-3">
+                <div class="small-box <?= $t['unpaid'] > 0 ? 'bg-warning' : 'bg-success' ?>">
+                    <div class="inner text-center">
+                        <h4>৳ <?= number_format($t['total_earned'], 2) ?></h4>
+                        <p><?= esc($t['name']) ?></p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-user-tie"></i>
+                    </div>
+
+                    <!-- Make button more visible -->
+                    <div class="text-center mb-2 mt-1">
+                        <form method="post" action="<?= base_url('admin/reset_amount/' . $t['id']) ?>">
+                            <?= csrf_field() ?>
+                            <button type="submit" class="btn btn-sm btn-danger btn-block">
+                                Mark as Paid <i class="fas fa-check"></i>
+                            </button>
+                        </form>
+                    </div>
+
                 </div>
-                <div class="icon">
-                    <i class="fas fa-user-tie"></i>
-                </div>
-                <form method="post" action="<?= base_url('admin/reset_amount/' . $t['id']) ?>">
-                    <?= csrf_field() ?>
-                    <button type="submit" class="small-box-footer btn btn-sm btn-light btn-block">
-                        Mark as Paid <i class="fas fa-check"></i>
-                    </button>
-                </form>
             </div>
-        </div>
-    <?php endforeach; ?>
-</div>
+        <?php endforeach; ?>
+    </div>
 </div>
 
 <?= $this->endSection() ?>
