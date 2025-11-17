cef86fe7341ad44b25b03f1767aaec9554024ca6
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-11-17 22:48:27 +0600

    add .env and App.php for new project

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *s
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://mulss.edu.bd/';
+    public string $baseURL = 'https://jpscac.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
