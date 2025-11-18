57ba6091c8e7ffabc6cf17a4cb6e0bf4caf8737e
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-11-18 08:12:31 +0600

    school name and contact

--- app/Views/layouts/base-structure/footer.php
+++ app/Views/layouts/base-structure/footer.php
@@ -6,8 +6,8 @@
             <div class="col-md-4">
                 <h5>Contact Us</h5>
                 <p>Keshabpur, Jessor</p>
-                <p>Email: s115832mul@gmail.com</p>
-                <p>Phone: +8801309115832</p>
+                <p>Email: email@gmail.com</p>
+                <p>Phone: +phone</p>
             </div>
 
             <!-- Quick Links -->
@@ -26,8 +26,8 @@
                 <h5>Our Location</h5>
                 <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32110.368378368563!2d89.19764134292312!3d22.919044334715988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff6c3a2047b9a1%3A0xd4f2101c4df0c706!2z4Kau4KeC4Kay4KaX4KeN4Kaw4Ka-4KauIOCmruCmvuCmp-CnjeCmr-CmruCmv-CmlSDgpqzgpr_gpqbgp43gpq_gpr7gprLgp58!5e0!3m2!1sbn!2sbd!4v1746510330645!5m2!1sbn!2sbd" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32110.368378368563!2d89.19764134292312!3d22.919044334715988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff6c3a2047b9a1%3A0xd4f2101c4df0c706!2z4Kau4KeC4Kay4KaX4KeN4Kaw4Ka-4KauIOCmruCmvuCmp-CnjeCmr-CmruCmv-CmlSDgpqzgpr_gpqbgp43gpq_gpr7gprLgp58!5e0!3m2!1sbn!2sbd!4v1746510330645!5m2!1sbn!2sbd" 
-                width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32110.368378368563!2d89.19764134292312!3d22.919044334715988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff6c3a2047b9a1%3A0xd4f2101c4df0c706!2z4Kau4KeC4Kay4KaX4KeN4Kaw4Ka-4KauIOCmruCmvuCmp-CnjeCmr-CmruCmv-CmlSDgpqzgpr_gpqbgp43gpq_gpr7gprLgp58!5e0!3m2!1sbn!2sbd!4v1746510330645!5m2!1sbn!2sbd"
+                    width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
 
