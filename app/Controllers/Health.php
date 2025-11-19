b7d40366f92b5ec454c45ce29f190ef80d252ff4
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-11-19 09:40:09 +0600

    add drugs 4

--- app/Controllers/Health.php
+++ app/Controllers/Health.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controllers;
 
 use App\Models\DrugsModel;
@@ -10,15 +9,6 @@ class Health extends BaseController
     {
         $drugModel = new DrugsModel();
 
-        $data['title'] = 'Prescription';
-        $data['activeSection'] = 'health';
-
-        // Navbar items so AdminLTE layout doesn't break
-        $data['navbarItems'] = [
-            ['label' => 'Prescription', 'url' => base_url('prescription')],
-        ];
-
-        // Fetch the drug list
         $data['drugs'] = $drugModel->findAll();
 
         return view('health/prescription', $data);
