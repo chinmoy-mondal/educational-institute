9ff75b048f7981053d213a1789d51ed22e3ffd10
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-11-23 07:55:09 +0600

    add drugs

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -158,6 +158,8 @@ $routes->post('admin/attendance/save', 'Dashboard::saveAttendance');
 $routes->get('prescription', 'Health::prescription');
 $routes->get('search-drugs', 'Health::searchDrugs');
 
+$routes->post('save-drug', 'Health::save');
+
 $routes->set404Override(function () {
     $controller = new \App\Controllers\ErrorController();
     return $controller->show404();
