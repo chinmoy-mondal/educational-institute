83fc71a18c44e1e2bfa837b96a3e8df33c91ce5f
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-12-12 12:38:59 +0600

    delete attendance for teacher and add new

--- app/Database/Migrations/2025-12-12-063707_CreateTeacherAttendanceTable.php
+++ app/Database/Migrations/2025-12-12-063707_CreateTeacherAttendanceTable.php
@@ -10,38 +10,32 @@ class CreateTeacherAttendanceTable extends Migration
     {
         $this->forge->addField([
             'id' => [
-                'type' => 'INT',
-                'constraint' => 11,
-                'unsigned' => true,
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
                 'auto_increment' => true,
             ],
-
             'teacher_id' => [
-                'type' => 'INT',
+                'type'       => 'INT',
                 'constraint' => 11,
-                'unsigned' => true,
+                'unsigned'   => true,
             ],
-
             'remark' => [
-                'type' => 'ENUM',
-                'constraint' => ['Present', 'Absent', 'Late', 'Leave'],
-                'default' => 'Present'
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => true,
             ],
-
-            'created_at' => [
-                'type' => 'DATETIME',
-                'null' => true,
-            ],
-
-            'updated_at' => [
-                'type' => 'DATETIME',
-                'null' => true,
-            ],
+            'created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ]);
 
-        $this->forge->addPrimaryKey('id');
+        // Primary key
+        $this->forge->addKey('id', true);
+
+        // Foreign key
         $this->forge->addForeignKey('teacher_id', 'users', 'id', 'CASCADE', 'CASCADE');
 
+        // Create table
         $this->forge->createTable('teacher_attendance');
     }
 
