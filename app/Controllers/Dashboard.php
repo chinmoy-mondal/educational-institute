b7548fa51215e9c84e53b2a09584bb575c30c38d
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-12-28 00:27:00 +0600

    new code for grade and grade point 5

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1863,7 +1863,7 @@ class Dashboard extends Controller
         }
         unset($row); // 🔒 VERY IMPORTANT
 
-        return view('result/test_result', [
+        return view('dashboard/test_result', [
             'marksheet' => $marksheetNumeric
         ]);
     }
