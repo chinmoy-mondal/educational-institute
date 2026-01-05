a573fda7a867c54fb93c0e6757c072062de0dc03
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-01-05 21:15:43 +0600

    user collection posting migrattion

---
+++ app/Database/Migrations/2026-01-05-151313_CreateUserCollectionsPayTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUserCollectionsPayTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'user_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+                'null'       => false,
+            ],
+            'amount_paid' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '12,2',
+                'default'    => 0.00,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); // primary key
+        $this->forge->createTable('user_collections');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user_collections');
+    }
+}
