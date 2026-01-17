331b313ff97df95e7c6a569eda91b1835c9327ed
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-01-17 10:26:14 +0600

    Jhenaidah Cadet Coaching to page name 1

--- app/Views/layouts/base-structure/header.php
+++ app/Views/layouts/base-structure/header.php
@@ -15,7 +15,9 @@
 <!--  Second Line: Logo, Institute Name & Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url(''); ?>"><img src="<?= base_url('public/assets/img/logo.jpg'); ?>">Jhenaidah Public School & Collage</a>
+        <a class="navbar-brand" href="<?= base_url(''); ?>"><img
+                src="<?= base_url('public/assets/img/logo.jpg'); ?>">Jhenaidah Cadet Coaching
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -61,7 +63,8 @@
                         <li><a class="dropdown-item" href="<?= base_url('student'); ?>">Student Portal</a></li>
                         <li><a class="dropdown-item" href="<?= base_url('student_stat'); ?>">Student Statistics</a></li>
                         <li><a class="dropdown-item" href="<?= base_url('attendance'); ?>">Attendance</a></li>
-                        <li><a class="dropdown-item" href="<?= base_url('attendanceStats'); ?>">Attendance Statistics</a></li>
+                        <li><a class="dropdown-item" href="<?= base_url('attendanceStats'); ?>">Attendance
+                                Statistics</a></li>
                     </ul>
                 </li>
 
@@ -69,7 +72,8 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Exam</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="<?= base_url('print-admit-form'); ?>">Exams & Results</a></li>
+                        <li><a class="dropdown-item" href="<?= base_url('print-admit-form'); ?>">Exams & Results</a>
+                        </li>
                     </ul>
                 </li>
 
