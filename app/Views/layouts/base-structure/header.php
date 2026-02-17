28eff5e84aec1ab7e0683a5dea46d424cc79c31d
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-02-18 05:17:21 +0600

    for mobile and desktop view change 3

--- app/Views/layouts/base-structure/header.php
+++ app/Views/layouts/base-structure/header.php
@@ -26,7 +26,7 @@
 
             <!-- Mobile Short Name -->
             <span class="fs-6 d-inline d-md-none">
-                Dattanagar S.M.F. Secondary School
+                Dattanagar S.M.F.S. School
             </span>
 
         </a>
