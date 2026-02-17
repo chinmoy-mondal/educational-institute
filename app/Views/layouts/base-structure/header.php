b8651b2b4f9f7ce72ae3a67a9800005923426166
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-02-18 05:09:10 +0600

    add

--- app/Views/layouts/base-structure/header.php
+++ app/Views/layouts/base-structure/header.php
@@ -15,8 +15,10 @@
 <!--  Second Line: Logo, Institute Name & Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url(''); ?>"><img
-                src="<?= base_url('public/assets/img/logo.jpg'); ?>"><?= esc(env('school.name')) ?></a>
+        <a class="navbar-brand" href="<?= base_url(''); ?>">
+            <img src="<?= base_url('public/assets/img/logo.jpg'); ?>" height="40">
+            <span class="fs-6"><?= esc(env('school.name')) ?></span>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
