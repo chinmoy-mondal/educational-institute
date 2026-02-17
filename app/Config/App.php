45e128c4dc94b970e534762e721d3b3615e67174
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-02-17 09:35:54 +0600

    base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *s
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://mulss.edu.bd/';
+    public string $baseURL = 'http://dsmfss.edu.bd/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
