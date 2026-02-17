12decae96a1d72367d0033634b26304532ecf108
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-02-18 05:21:42 +0600

    map location 1

--- app/Views/layouts/base-structure/footer.php
+++ app/Views/layouts/base-structure/footer.php
@@ -28,8 +28,8 @@
 
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d228.88491672729666!2d88.8230319!3d23.3824766!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fed57ee9d2e209%3A0x9d6017e1bba86696!2sDattanagar%20SM%20Farm%20Secondary%20School!5e0!3m2!1sen!2sbd!4v1771370373344!5m2!1sen!2sbd"
-                    style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                </iframe>
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
 
