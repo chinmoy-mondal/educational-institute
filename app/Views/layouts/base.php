6aa8bfb7803e81a61d6c68bf1b8205f486f66b69
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-02-17 10:02:57 +0600

    change bage url and base institute name 1

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $school_name ?></title>
+    <title><?= config('App')->schoolName ?></title>
 
     <link rel="stylesheet" href="<?= base_url('public/assets/css/bootstrap.min.css'); ?>">
     <script src="<?= base_url('public/assets/js/bootstrap.bundle.min.js') ?>"></script>
