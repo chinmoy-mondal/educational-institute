ef500ed7c0fb353e05bc1b2b9fdd10d42e62e762
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-03-18 22:13:38 +0600

    add migration for holiday

---
+++ app/Database/Migrations/2026-03-18-160309_CreateHolidaysTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateHolidaysTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'desc' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'start_date' => [
+                'type' => 'DATE',
+            ],
+            'end_date' => [
+                'type' => 'DATE',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('holidays');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('holidays');
+    }
+}
