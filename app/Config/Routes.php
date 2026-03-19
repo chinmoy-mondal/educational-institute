96ca978f6c29573baf25f64405893e91c85cf657
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-03-19 10:15:30 +0600

    add and edit form 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -81,9 +81,13 @@ $routes->post('calendar/update', 'Dashboard::updateEvent');
 $routes->post('calendar/delete', 'Dashboard::deleteEvent');
 
 $routes->get('admin/holiday', 'Dashboard::holiday');
+
 $routes->get('admin/holiday/add', 'Dashboard::addHolidayForm');
 $routes->post('admin/holiday/add', 'Dashboard::saveHoliday');
+
 $routes->get('admin/holiday/edit/(:num)', 'Dashboard::editHoliday/$1');
+$routes->post('admin/holiday/update/(:num)', 'Dashboard::updateHoliday/$1');
+
 $routes->get('admin/holiday/delete/(:num)', 'Dashboard::deleteHoliday/$1');
 
 $routes->get('ad_teacher_list', 'Dashboard::teachers');
