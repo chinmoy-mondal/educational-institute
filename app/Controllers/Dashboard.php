fdfdc31f69a5fbae1796e200a4bd344dbfec6385
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-03-22 14:25:17 +0600

    add user id 7

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -698,6 +698,7 @@ class Dashboard extends Controller
 
         // 🔥 Get data
         $this->data['leaves'] = $builder
+            ->where('leaves.status', 'Pending') // 👈 only pending
             ->orderBy('leaves.id', 'DESC')
             ->findAll();
 
