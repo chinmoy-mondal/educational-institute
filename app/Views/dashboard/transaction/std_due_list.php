9ac628659efa9c2a2aa2fb822c683fa33fb0e180
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-04-18 18:16:11 +0600

    test section 4

--- app/Views/dashboard/transaction/std_due_list.php
+++ app/Views/dashboard/transaction/std_due_list.php
@@ -94,7 +94,7 @@
                             $discount = $paymentSummary[$sid]['discount'] ?? 0;
                             $get = $paid - $discount;
                             $netDue = $totalFee - $paid;
-
+                            echo $netDue;
                             if ($dueType == 'due' && $netDue <= 0) {
                                 continue;
                             }
