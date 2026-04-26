f6add53a43fe8ae5c3f8363b02ba6290eba904f6
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-04-26 21:55:05 +0600

    add stat 7

--- app/Views/public/idCard.php
+++ app/Views/public/idCard.php
@@ -80,7 +80,7 @@ body {
                     <span class="mx-1"></span>
                     <i class="fas fa-tint"></i> <?= esc($student['blood_group']) ?>
                 </p>
-                <p class="mb-0"><i class="fas fa-phone me-2"></i><?= esc($student['phone']) ?></p>
+                <p class="mb-0"><i class="fas fa-phone me-2"></i><?= esc($student['class']) ?></p>
 
             </div>
 
