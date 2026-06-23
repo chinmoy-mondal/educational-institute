c7be11bcb0438666d372ff67fba2223995bf2f44
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-06-23 23:27:16 +0600

    update exam rouiten 12

--- app/Views/dashboard/exam/create_exam_routine.php
+++ app/Views/dashboard/exam/create_exam_routine.php
@@ -13,6 +13,7 @@
             <form action="<?= base_url('admin/exam-routine/store') ?>" method="post">
                 <?= csrf_field() ?>
 
+                <!-- TITLE + DESCRIPTION -->
                 <div class="row">
 
                     <div class="col-md-6">
@@ -27,28 +28,29 @@
 
                 </div>
 
+                <!-- CLASS + SUBJECT + CATEGORY + COLOR -->
                 <div class="row mt-3">
 
+                    <!-- CLASS -->
                     <div class="col-md-3">
                         <label>Class</label>
-                        <select name="class" class="form-control">
+                        <select name="class" id="class" class="form-control">
+                            <option value="">Select Class</option>
                             <?php for ($i = 6; $i <= 10; $i++): ?>
                             <option value="<?= $i ?>">Class <?= $i ?></option>
                             <?php endfor; ?>
                         </select>
                     </div>
 
+                    <!-- SUBJECT (DYNAMIC) -->
                     <div class="col-md-3">
                         <label>Subject</label>
-                        <select name="subject" class="form-control">
-                            <?php foreach ($subjects as $sub): ?>
-                            <option value="<?= $sub['id'] ?>">
-                                <?= $sub['subject'] ?>
-                            </option>
-                            <?php endforeach; ?>
+                        <select name="subject" id="subject" class="form-control">
+                            <option value="">Select Subject</option>
                         </select>
                     </div>
 
+                    <!-- CATEGORY -->
                     <div class="col-md-3">
                         <label>Sub Category</label>
                         <select name="subcategory" class="form-control">
@@ -59,6 +61,7 @@
                         </select>
                     </div>
 
+                    <!-- COLOR -->
                     <div class="col-md-3">
                         <label>Color</label>
                         <input type="color" name="color" class="form-control" value="#007bff">
@@ -66,6 +69,7 @@
 
                 </div>
 
+                <!-- DATE TIME -->
                 <div class="row mt-3">
 
                     <div class="col-md-3">
@@ -90,6 +94,7 @@
 
                 </div>
 
+                <!-- BUTTON -->
                 <div class="mt-4">
                     <button class="btn btn-success">Save</button>
                     <a href="<?= base_url('admin/exam-routine') ?>" class="btn btn-secondary">Back</a>
@@ -101,5 +106,45 @@
     </div>
 
 </div>
+
+<!-- ================= AJAX SCRIPT ================= -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+$('#class').on('change', function() {
+
+    let classId = $(this).val();
+
+    if (classId === "") {
+        $('#subject').html('<option value="">Select Subject</option>');
+        return;
+    }
+
+    $('#subject').html('<option>Loading...</option>');
+
+    $.ajax({
+        url: "<?= base_url('admin/get-subjects') ?>",
+        type: "GET",
+        data: {
+            class: classId
+        },
+        dataType: "json",
+        success: function(data) {
+
+            let options = '<option value="">Select Subject</option>';
+
+            data.forEach(function(item) {
+                options += `<option value="${item.id}">${item.subject}</option>`;
+            });
+
+            $('#subject').html(options);
+        },
+        error: function() {
+            $('#subject').html('<option value="">Error loading subjects</option>');
+        }
+    });
+
+});
+</script>
 
 <?= $this->endSection() ?>
