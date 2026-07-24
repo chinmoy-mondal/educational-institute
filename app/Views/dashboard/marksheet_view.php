aea94a5bdacb62a6613bc6a59fec3e896f88b1d2
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-24 16:42:08 +0600

    if faili subject is more then 1 show total grate 0

--- app/Views/dashboard/marksheet_view.php
+++ app/Views/dashboard/marksheet_view.php
@@ -783,7 +783,17 @@ if (!is_null($roll)) {
 
                 <th class="text-end"><?= $totalMarks ?></th>
                 <th colspan="2">
-                    <?= $subjectCount > 0 ? (($golden_gpa = ($totalGPA + $forthGPA) / $subjectCount) > 5 ? '5.00*' : number_format($golden_gpa, 2)) : '0.00' ?>
+                    <?php
+          if ($totalFailed > 0) {
+            echo '0.00';
+          } else {
+            echo $subjectCount > 0
+              ? (($golden_gpa = ($totalGPA + $forthGPA) / $subjectCount) > 5
+                ? '5.00*'
+                : number_format($golden_gpa, 2))
+              : '0.00';
+          }
+          ?>
                 </th>
             </tr>
         </tfoot>
