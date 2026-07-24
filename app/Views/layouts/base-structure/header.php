4069d9a387d9a50a33938be01ef61ff3e7bc05bd
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-24 14:41:16 +0600

    update url

--- app/Views/layouts/base-structure/header.php
+++ app/Views/layouts/base-structure/header.php
@@ -15,7 +15,8 @@
 <!--  Second Line: Logo, Institute Name & Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url(''); ?>"><img src="<?= base_url('public/assets/img/logo.jpg'); ?>">Mulgram Secondary School</a>
+        <a class="navbar-brand" href="<?= base_url(''); ?>"><img
+                src="<?= base_url('public/assets/img/logo.jpg'); ?>">Mulgram Secondary School</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -61,15 +62,16 @@
                         <li><a class="dropdown-item" href="<?= base_url('student'); ?>">Student Portal</a></li>
                         <li><a class="dropdown-item" href="<?= base_url('student_stat'); ?>">Student Statistics</a></li>
                         <li><a class="dropdown-item" href="<?= base_url('attendance'); ?>">Attendance</a></li>
-                        <li><a class="dropdown-item" href="<?= base_url('attendanceStats'); ?>">Attendance Statistics</a></li>
+                        <li><a class="dropdown-item" href="<?= base_url('attendanceStats'); ?>">Attendance
+                                Statistics</a></li>
                     </ul>
                 </li>
 
                 <!-- Events -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Exam</a>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Results</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="<?= base_url('print-admit-form'); ?>">Exams & Results</a></li>
+                        <li><a class="dropdown-item" href="<?= base_url('select-marksheet'); ?>">Results</a></li>
                     </ul>
                 </li>
 
