8b348a0b546a1f9ac239b8cdb9714b480b47d8f1
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-26 22:20:54 +0600

    exam name change

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3104,7 +3104,7 @@ class Dashboard extends Controller
         if ($exam === 'Annual Exam') {
             // Annual exam goes to full result function
             return $this->test_result($studentId, $year, $exam, $view);
-        } elseif (in_array($exam, ['Pre-Test Exam', 'Half-Yearly', 'Test Exam'])) {
+        } elseif (in_array($exam, ['Pre-Test Exam', 'Half Yearly Exam', 'Test Exam'])) {
             // Other exams go to single exam function
             return $this->test_result_single_exam($studentId, $year, $exam, $view);
         }
