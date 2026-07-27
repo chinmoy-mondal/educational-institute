11539a6545ee40adcfb388bb87e8e175adf29c85
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-27 18:13:40 +0600

    test 3

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2539,7 +2539,6 @@ class Dashboard extends Controller
 
     public function test_result_single_exam($studentId = null, $year = null, $exam = null, $view = null)
     {
-        echo "check test sin";
         if (!$studentId || !$year || !$exam) {
             return "Student ID, Year and Exam are required";
         }
@@ -2621,9 +2620,9 @@ class Dashboard extends Controller
                 $practical,
                 $percentage
             );
-            // echo "<pre>";
-            // print_r($gradeInfo);
-            // echo "</pre>";
+            echo "<pre>";
+            print_r($gradeInfo);
+            echo "</pre>";
 
             $marksheetNumeric[] = [
                 'subject'   => $row['subject'],
