c1c5e3a6ed7044cbd2241e3ef0e9e4280e466ea6
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-27 18:20:24 +0600

    test 9

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2539,7 +2539,10 @@ class Dashboard extends Controller
 
     public function test_result_single_exam($studentId = null, $year = null, $exam = null, $view = null)
     {
-        echo "test";
+        echo "student id " . $studentId . "<br>";
+        echo "year " . $year . "<br>";
+        echo "exam " . $exam . "<br>";
+        echo "view " . $view . "<br>";
         if (!$studentId || !$year || !$exam) {
             return "Student ID, Year and Exam are required";
         }
