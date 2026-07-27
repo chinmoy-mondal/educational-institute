0b3b851e50f138709ba66f9c62971c7ec306aa1c
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-27 18:09:25 +0600

    saveRankingFromResul test

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2750,9 +2750,9 @@ class Dashboard extends Controller
         $exam           = $data['exam'];
         $year           = $data['year'];
 
-        // echo "<pre>";
-        // print_r($marksheet);
-        // echo "</pre>";
+        echo "<pre>";
+        print_r($marksheet);
+        echo "</pre>";
 
         if ($exam == 'Annual Exam') {
             $total_fail = 0;
