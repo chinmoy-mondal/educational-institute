8a053e2b58ca18d00c82ba474139fa5171f4eb14
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-07-27 12:45:33 +0600

    for mark giving 169, as well as 169* both student come

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1338,9 +1338,11 @@ class Dashboard extends Controller
         }
 
         $students = $this->studentModel
-            ->where("FIND_IN_SET(" . (int)$subjectId . ", assign_sub) >", 0, false)
+            ->groupStart()
+            ->where("FIND_IN_SET('" . $subjectId . "', assign_sub) >", 0, false)
+            ->orWhere("FIND_IN_SET('" . $subjectId . "*', assign_sub) >", 0, false)
+            ->groupEnd()
             ->where('permission', 0)
-            ->where('class <=', 10)
             ->orderBy('CAST(roll AS UNSIGNED)', 'ASC', false)
             ->findAll();
 
