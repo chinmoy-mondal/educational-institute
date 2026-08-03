5ccb1f6246d8b862ea5a05d472642a2a47a6ea54
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-08-03 06:16:24 +0600

    add test code

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1821,6 +1821,11 @@ class Dashboard extends Controller
             ->orderBy('CAST(roll AS UNSIGNED)', 'ASC', false)
             ->findAll();
 
+            echo "<pre>";
+            print_r($students);
+            echo "</pre>";
+        
+
         // 🔄 Load existing results for this teacher and subject
         $results = $this->resultModel
             ->where('teacher_id', $userId)
