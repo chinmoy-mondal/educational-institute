3796b9f182ce3a2989d4943aaaea2dd5f48aaa92
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-09-19 17:44:58 +0600

    current amount based on unit calculation 5

--- app/Views/dashboard/transaction/payStudentRequest.php
+++ app/Views/dashboard/transaction/payStudentRequest.php
@@ -75,6 +75,7 @@
                                         $payCount = floor($currentMonth / $interval);
                                     }
                                 }
+                                $payCount = max(1, $payCount);
 
                                 $payAmount = $payCount * $amount;
 
@@ -84,7 +85,7 @@
                             <tr>
                                 <td><?= $sl++ ?></td>
                                 <td><?= esc($f['title']) ?></td>
-                                <td><?= $unit && $amount ? esc($unit . ' times/yr. But now ' . $payCount . ' x '   . $amount) : '-' ?>
+                                <td><?= $unit && $amount ? esc($unit . '/Yr. Now ' . $payCount . ' x '   . $amount) : '-' ?>
                                 </td>
                                 <td>
                                     <input type="hidden" name="fee_id[<?= $index ?>]" value="<?= esc($f['id']) ?>">
