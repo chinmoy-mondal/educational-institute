9b28c123c6f2e5d5110f68eb24be54bff9d68199
Chinmoy Mondal <chinmoymondal791@gmail.com>
2026-09-19 11:50:27 +0600

    add test for veriable 1

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -5799,6 +5799,10 @@ class Dashboard extends Controller
         // ✅ ONLY SECTION
         $class = $this->request->getGet('class');
 
+        echo "receive result " . $class . "<br>";
+        // echo "receive result " . $feesData . "<br>";
+        // echo "receive result " . $unitsData . "<br>";
+
         $this->data['selectedClass'] = $class;
 
         // Fee titles
@@ -5854,16 +5858,14 @@ class Dashboard extends Controller
         $class   = $this->request->getPost('class');
         $feesData  = $this->request->getPost('fees');
         $unitsData = $this->request->getPost('unit');
-        echo "receive result " . $class . "<br>";
-        echo "receive result " . $feesData . "<br>";
-        echo "receive result " . $unitsData . "<br>";
+
 
         if (!$class) {
-            // return redirect()->back()->with('error', 'Please select a section before saving.');
+            return redirect()->back()->with('error', 'Please select a section before saving.');
         }
 
         if (empty($feesData)) {
-            // return redirect()->back()->with('error', 'No fee amounts to save.');
+            return redirect()->back()->with('error', 'No fee amounts to save.');
         }
 
         $amountModel = new FeesAmountModel();
@@ -5901,7 +5903,7 @@ class Dashboard extends Controller
             }
         }
 
-        // return redirect()->back()->with('success', 'Fees updated successfully!');
+        return redirect()->back()->with('success', 'Fees updated successfully!');
     }
 
     public function payStudentRequest($id)
